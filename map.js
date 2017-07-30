534a743ae944c48ec162bcd4ca00b085367fa92e
Patrick McCaffrey <patrick.mccaffrey.19@gmail.com>
2017-07-30 12:17:16 +1200

    :neckbeard:

--- map.js
+++ map.js
@@ -1,36 +1,49 @@
-// Initialize the platform object:
-var platform = new H.service.Platform({
-'app_id': 'DQ9SXnZ2AO94IjXYgTVY',
-'app_code': 'IVoPXJITL7TIeLAVk4dyhQ'
-});
-// Obtain the default map types from the platform object
-var maptypes = platform.createDefaultLayers();
-// Instantiate (and display) a map object:
-var map = new H.Map(
-document.getElementById('mapContainer'),
-maptypes.normal.map,
-{
-  zoom: 7,
-  center: { lng: 174.763336, lat: -36.848461 }
-});
+// Initialize map obj
+function initMap(){
+  var platform = new H.service.Platform({
+  'app_id': 'DQ9SXnZ2AO94IjXYgTVY',
+  'app_code': 'IVoPXJITL7TIeLAVk4dyhQ'
+  });
+  // Obtain the default map types from the platform object
+  var maptypes = platform.createDefaultLayers();
+  // Instantiate (and display) a map object:
+  var map = new H.Map(
+  document.getElementById('mapContainer'),
+  maptypes.normal.map,
+  {
+    zoom: 7,
+    center: { lng: 174.763336, lat: -36.848461 }
+  });
 
-// Enable the event system on the map instance:
-var mapEvents = new H.mapevents.MapEvents(map);
+  // Enable the event system on the map instance:
+  var mapEvents = new H.mapevents.MapEvents(map);
 
-// adds default map behavior
-var behavior = new H.mapevents.Behavior(mapEvents);
+  // adds default map behavior
+  var behavior = new H.mapevents.Behavior(mapEvents);
 
+  return map;
+}
+
+var map = initMap()
 
 var district_style = {
   strokeColor: 'black',
-  lineWidth: 5,
+  lineWidth: 2,
   lineJoin: 'bevel'
 }
 
 const currentRenderedDistricts = [];
 var boundariesGeoJSON = getBoundaryPolygons();
 
+function colourFromNumber(value){
+    // 1 = RED
+    // 0 = GREEN
 
+    //value from 0 to 1
+    var hue=((1-value)*120).toString(10);
+    return ["hsla(",hue,",100%,50%, 0.5)"].join("");
+
+}
 
 // area RENDER Functions
 
@@ -72,30 +85,33 @@ function findDistrictObject(name_string){
 }
 
 
+function initBoundaryShapes(){
+  boundariesGeoJSON.forEach(function(city, i){
 
-boundariesGeoJSON.forEach(function(city, i){
+    currentRenderedDistricts[i] = addDistrict(city);
 
-  currentRenderedDistricts[i] = addDistrict(city);
-
-})
-
-
-
+  });
+}
 
 
 function updateHeatMap(){
 
 
+  currentRenderedDistricts.forEach(function(map_object){
+
+    map_object.setStyle(
+      Object.assign(district_style,
+      {"fillColor": colourFromNumber(Math.random().toFixed(1))})
+      )
+
+  })
 
 }
 
-function colourFromNumber(number){
+initBoundaryShapes()
 
 
 
-
-  return 'rgba(255, 255, 255, 0.5';
-}
 
 
 
